a9bf70ffeba6128afc32df6255bb868f573a29d5
john <john.d.leonetti@gmail.com>
2021-05-26 12:45:00 -0700

    commit tree eb907cdd3fe87b653a2e2f02bbfc9c4493a8a814

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+//const names = ['Lisa', 'Kaitlin', 'Jan' ]
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+      console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return names;
+}
+
+// function countDown(countdown) {
+//     let countdown = 10;
+//     while (countdown > 0) {
+//         console.log(countdown++);
+//     }
+//     countDown(10);
+// }
